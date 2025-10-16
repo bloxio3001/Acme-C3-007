3d8f5ca94d219426d5c4f8d5814724d80af5f27a
bloxio3001 <adrianporras.azahares@gmail.com>
2025-10-16 04:23:33 +0200

    Add manager accounts

--- src/main/java/acme/entities/airlineManagers/AirlineManager.java
+++ src/main/java/acme/entities/airlineManagers/AirlineManager.java
@@ -10,7 +10,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -27,7 +27,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidAirlineManager
-public class AirlineManager extends AbstractEntity {
+public class AirlineManager extends AbstractRole {
 
 	/**
 	 * 
